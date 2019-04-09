7a8ea605a3141bb6a8e30d323c8b05913f209b71
Kevin <kevin.ceriati@le-campus-numerique.fr>
2019-04-09 14:01:53 +0200

    fin exo-4

--- Exos-JQuery/exo-4/script.js
+++ Exos-JQuery/exo-4/script.js
@@ -1,7 +1,46 @@
 console.log("exercice 4");
 
-$('#signUp').click(function(){
-    window.alert('Merci conard').val();;  
-});
 
-$('#email')
+$(document).ready(function () {
+
+
+
+    $('.btn-danger').click(function () {
+        var $mail = $('input').val();
+        window.alert('Merci ' + $mail);
+    });
+
+
+
+    $('.navbar-nav a:eq(1)').on('dblclick', function () {
+        $(this).hide();
+    });
+
+
+
+    $('img').on('click', function () {
+        var $achat = parseInt($(this).closest('.panel').children('.panel-footer').text().replace(/[^\d]/g, ""));
+        var $glyph = $('.glyphicon-shopping-cart');
+        var $li = $('.glyphicon-shopping-cart').parent();
+        var $quantity = parseInt($("a:contains('Cart')").text().replace(/[^\d]/g, ""));
+        var $total = ($quantity > 0) ? $achat + $quantity : $achat;
+        $li.text('Cart' + $total);
+        $li.prepend($glyph);
+    })
+
+
+
+
+    $('img').on('mouseenter', function () {
+        var article = $(this).closest('.panel').children('.panel-footer').text();
+        //console.log('L\'utilisateur regarde ' + article);
+    });
+
+    $('form input').on('keydown', function () {
+        console.log($('form input').val());
+
+    });
+
+
+
+});
