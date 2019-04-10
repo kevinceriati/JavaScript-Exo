b0a793e1fc878c75893870502d6d921b2b1e499e
Kevin <kevin.ceriati@le-campus-numerique.fr>
2019-04-10 11:14:23 +0200

    fin de l'exo-6

--- Exos-JQuery/exo-6/script.js
+++ Exos-JQuery/exo-6/script.js
@@ -2,12 +2,37 @@ console.log("exercice 6");
 
 
 $(document).ready(function () {
-$('.btn-primary').on('click', function () {
-    var mail = $('#email').val();
-    var pwd = $('#password').val();
-    window.alert('Merci ' + mail);
+
+    $('.alert').hide();
+
+    $('.btn-primary').on('click', function () {
+        var  mailLog = 'hello@me.com';
+        var pwdLog = 'secret8';
+        var mail = $('#email').val();
+        var pwd = $('#password').val();
+        console.log(mail);
+        if ((mail === '') || !validateEmail(mail)) {
+            $('.alert').show().text('email invalide !')
+        }else{
+            if ((pwd === '') || (pwd.length < 6)) {
+                $('.alert').show().text('password invalide !')
+            }else{
+                $('.alert').hide();
+                window.alert('merci tapette')
+            }          
+        }
+        
+        if ((mail === mailLog) && (pwd === pwdLog)) {
+            $('.alert').show().text('Vous êtes connecté')
+        }
+    })
 });
 
+function validateEmail(mail) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(mail).toLowerCase());
+}
+ 
 
 
 
@@ -15,4 +40,3 @@ $('.btn-primary').on('click', function () {
 
 
 
-});
